14adb676db2a51b103cea5bed0f3eb81cdd823f3
crossdsection <chirkut.kr@gmail.com>
2018-09-11 18:02:59 +0530

    Base Code Planned

---
+++ change-file-prefixes-in-directory.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script to rename bulk files in Directory
+
+# $1 - directory
+# $2 - new prefix
+
+filenames="$(find $1 -name 'Wv*')"
+newPrefix="/";
+delimeter="/Wv";
+echo $newPrefix;
+echo $delimeter;
+for fileName in $filenames; 
+do     
+	echo "> [$fileName]";
+	result="$($fileName/$delimeter/$newPrefix//)";
+	echo "$result";
+	newFileName="${string/$delimeter/$newPrefix}";
+	echo $newFileName;
+done
+
